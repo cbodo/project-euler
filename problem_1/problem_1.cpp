1431d512ea917510cfa9ee1d7da76ee771b546f6
cbodo <142287245+cbodo@users.noreply.github.com>
2024-01-22 14:10:38 -0800

    problem 1

---
+++ problem_1/problem_1.cpp
@@ -0,0 +1,52 @@
+/* 
+Euler Project
+Problem 1
+Multiples of 3 or 5
+
+Description:
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+To Run:
+g++ your_program.cpp -o your_program
+./your_program
+
+*/
+
+#include <iostream>
+
+int sum_multiples_3_or_5 (int max) {
+    if(max == 0) return 0;
+
+    int sum = 0;
+    int i = 0;
+
+    // Use case i < max since instructions state program should sum all multiples /below/ 1000.
+    while(i < max)
+    {
+        if (i % 3 == 0 || i % 5 == 0) sum += i;
+        ++i;
+    }
+
+    return sum;
+}
+
+int main() {
+
+    int sum = 0;
+
+    sum = sum_multiples_3_or_5(1000);
+
+    std::cout << std::endl << "Euler Project" << std::endl << std::endl
+              << "Problem 1" << std::endl
+              << "Multiples of 3 or 5" << std::endl << std::endl
+              << "Description:" << std::endl << std::endl
+              << "If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23." << std::endl
+              << "Find the sum of all the multiples of 3 or 5 below 1000." << std::endl
+              << std::endl;
+
+    std::cout << "The sum of all multiples of 3 or 5 below 1000 is: " << sum << std::endl;
+
+    return 0;
+}
