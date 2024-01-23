3810ed0f1a873f3e9e3bfd9f2ac9bf7b584a0299
cbodo <142287245+cbodo@users.noreply.github.com>
2024-01-22 16:19:50 -0800

    problem_3

---
+++ problem_3/problem_3.cpp
@@ -0,0 +1,50 @@
+/*
+
+Craig Bodo
+Project Euler
+
+Problem 3
+Largest Prime Factor
+
+Description:
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143?
+
+To Run:
+
+g++ problem_3.cpp -o problem_3
+./problem_3
+
+*/
+#include <iostream>
+
+long largest_prime_factor (long num, long divisor);
+
+int main() {
+    long num = 600851475143;
+    long largest_prime = largest_prime_factor(num);
+
+    std::cout << std::endl
+              << "The largest prime factor of " << num << " is: " << largest_prime 
+              << std::endl 
+              << std::endl;
+
+    return 0;
+}
+
+// Finds the largest prime factor of a number, starting with the smallest divisor possible (2).
+long largest_prime_factor (long num, long divisor = 2) {
+    
+    // Base case: return when n is 1.
+    if(num == 1) { return divisor; }
+
+    // Divide num by the current smallest prime divisor
+    while(num % divisor != 0)
+    {
+        divisor++;
+    }
+
+    // Recursive call with updated num and divisor values.
+    return largest_prime_factor(num / divisor, divisor);
+}
