e43832af410fd442a5af44e65bf2eed3481b94da
cbodo <142287245+cbodo@users.noreply.github.com>
2024-01-27 13:23:59 -0800

    complete problem_19

--- problem_19/problem_19.cpp
+++ problem_19/problem_19.cpp
@@ -29,62 +29,59 @@ g++ problem_19.cpp -o problem_19
 */
 #include <iostream>
 #include <chrono>
+#include <cmath>
 
 void run_program ();
-int solution ();
-int date_counter(int start_year, int start_month, int start_day, int end_year, int end_month, int end_day);
-bool is_leap_year(int year);
-std::time_t get_time(int year, int day, int month);
+int sunday_counter(int start_year, int start_month, int start_day, int end_year, int end_month, int end_day);
+int get_day_of_week(int day_of_week, int month, int year_of_century, int zero_century);
 
 int main() {
     run_program();
     return 0;
 }
 
-int date_counter(int start_year, int start_month, int start_day, int end_year, int end_month, int end_day) {
+// Zeller's Congruence to determine the day of week based on given day, month, and year:
+// h = (q + floor((13(m+1))/5) + k + floor(k/4) + floor(j/4) - 2j ) % 7
+// where:
+//   h = day of week (0 = Sat, 1 = Sun, ..., 6 = Fri)
+//   q = day of the month
+//   m = month (3 = Mar, 4 = Apr, ..., 12 = Dec, 13 = Jan, 14 = Feb)
+//   k = year of century (year mod 100)
+//   j = zero-based century (year // 100)
+int get_day_of_week(int q, int m, int k, int j) {
+    return ((q + static_cast<int>(std::floor((13 * (m + 1)) / 5)) + k + static_cast<int>(std::floor(k / 4)) + static_cast<int>(std::floor(j / 4)) - (2 * j)) % 7);
+}
+
+int sunday_counter(int start_year, int end_year) {
     int sunday_count = 1;
     int current_year = start_year;
-    int current_month = start_month;
-    int current_day = start_day;
 
     while (current_year <= end_year) {
-        std::cout << current_year << '\n';
         for (int i = 0; i < 12; ++i) {
-            if (i == 1) {
-                if (is_leap_year(current_year)) {
-                    std::cout << (i+1 < 10 ? '0' : '\0') << i+1 << ": 29 days" << '\n';
+            if (i < 2) {
+                if(get_day_of_week(1, i+13, current_year%100, current_year/100) == 1) {
+                    sunday_count += 1;
                 }
-                std::cout << (i+1 < 10 ? '0' : '\0') << i+1 << ": 28 days" << '\n';
-            }
-            else if (i == 3 || i == 5 || i == 8 || i == 10 ) {
-                std::cout << (i+1 < 10 ? '0' : '\0')<< i+1 << ": 30 days" << '\n';
             }
             else {
-                std::cout << (i+1 < 10 ? '0' : '\0')<< i+1 << ": 31 days" << '\n';
+                if(get_day_of_week(1, i+1, current_year%100, current_year/100) == 1) {
+                    sunday_count += 1;
+                }
             }
-            
         }
         current_year++;
     }
-}
 
-bool is_leap_year(int year) {
-    return (year % 4 == 0) && ((year % 400 == 0) || (year % 100 != 0));
-}
-
-int solution () {
-    return 0;
+    return sunday_count;
 }
 
 void run_program () {
     // Start timestamp
     std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
 
-    date_counter(1901, 1, 1, 2000, 12, 31);
+    int result = sunday_counter(1901, 2000);
 
-    // int result = solution();
-
-    // std::cout << "\nSolution: " << result << '\n';
+    std::cout << "\nThe number of Sundays the fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000) is: " << result << '\n';
 
     // End timestamp
     std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
