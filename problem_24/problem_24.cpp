f0bd7c4b145c7c2a6d710f55669d9b2ded69d51b
cbodo <142287245+cbodo@users.noreply.github.com>
2024-01-30 18:56:39 -0800

    complete problem_24

--- problem_24/problem_24.cpp
+++ problem_24/problem_24.cpp
@@ -1,7 +1,7 @@
 /*
 
 Project Euler 
-Problem #24: Permutations
+Problem #24: Lexicographic Permutations
 Craig Bodo
 
 Description:
@@ -22,37 +22,43 @@ g++ -std=c++11 problem_24.cpp -o problem_24
 */
 #include <iostream>
 #include <vector>
-#include <set>
+#include <chrono>
 
 using namespace std;
+using namespace chrono;
 
 unsigned long long factorial(unsigned int n) {
-    if (n == 0 || n == 1) {
-        return 1;
-    } else {
-        unsigned long long result = 1;
-        for (unsigned int i = 2; i <= n; ++i) {
-            result *= i;
-        }
-        return result;
-    }
+    if(n == 0 || n == 1) return 1;
+    return n * factorial(n - 1);
+}
+
+void get_nth_lexicographic_permutation(vector<int> digits, int nth_index, int i = 0) {
+    if (digits.empty()) return;
+    int f = factorial(digits.size() - 1);
+    int k = nth_index/f;
+    nth_index %= f;
+    cout << digits[k];
+    digits.erase(digits.begin() + k);
+    get_nth_lexicographic_permutation(digits, nth_index, i + 1);
 }
 
 int main() {
-    vector<int> digits = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int millionth_index = 1000000;
+    cout << "\nProject Euler - Problem #24: Lexicographic Permutations\n";
 
-    int i = 0;
-    vector<int> temp = digits;
-    while (!temp.empty()) {
-        int f = factorial(temp.size() - 1);
-        int k = millionth_index/f;
-        millionth_index %= f;
-        cout << temp[k];
-        temp.erase(temp.begin() + k);
-        i++;
+    vector<int> digits = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int nth_index = 1000000;
+
+    cout << "\nThe " << nth_index << "th lexicographic permutation of the digits (";
+
+    for (int i = 0; i < digits.size(); ++i) {
+        cout << digits[i] << (i == digits.size() - 1 ? ") " : ", ");
     }
-    cout << endl;
+
+    cout << "is: ";
+
+    get_nth_lexicographic_permutation(digits, nth_index);
+
+    cout << endl << endl;
 
     return 0;
 }
