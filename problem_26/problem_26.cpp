bc96a55ba8d2e1f7c865d641fa91729e48588865
cbodo <142287245+cbodo@users.noreply.github.com>
2024-02-01 13:58:12 -0800

    complete problem_26

--- problem_26/problem_26.cpp
+++ problem_26/problem_26.cpp
@@ -35,46 +35,57 @@ g++ problem_26.cpp -o problem_26
 
 using namespace std;
 
+// Function to find the denominator (d) with the longest recurring cycle where d < n
 int find_longest_recurring_cycle(int n) {
     int longest_cycle = 0;
+    int d_with_longest_cycle = 0;
 
+    // Iterate all denominators from 2 to n-1
     for (int d = 2; d < n; ++d) {
-        vector<int> remainders(d, -1);
+        // Vector to store remainders 
+        vector<int> remainders(d);
         int numerator = 1;
+        // Position of current remainder
         int position = 0;
 
-        while (numerator != 0 && remainders[numerator] == -1) {
+        // Divide until numerator = 0 or repeating remainder is found
+        while (numerator != 0 && remainders[numerator] == 0) {
+            // Store current remainder's position
             remainders[numerator] = position++;
+            // Multiply numerator by 10 for next division step
             numerator *= 10;
+            // Calculate next remainder
             numerator %= d;
         }
 
         if (numerator != 0) {
-            int cycle_length = position - remainders[numerator];
+            // Calculate length of recurring cycle
+            int cycle_length = position - remainders[numerator] + 1;
+
+            // If repeating remainder is found and cycle length > longest cycle,
             if (cycle_length > longest_cycle) {
+                // Update longest cycle with current cycle
                 longest_cycle = cycle_length;
+                // Update d with current denominator
+                d_with_longest_cycle = d;
             }
         }
     }
-    return longest_cycle;
+    return d_with_longest_cycle;
 }
 
 int main() {
     
     cout << endl << "Project Euler - Problem #26: Reciprocal Cycles" << endl << endl;
 
+    int n = 1000;
 
-    cout << "The value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part is: "
+    // Run function with n = 10 to show proof in example, then run with n = 1000.
+    cout << "Longest recurring cycle for 1/d where d < " << 10 << " is: " << find_longest_recurring_cycle(10) 
+         << endl
+         << "Longest recurring cycle for 1/d where d < " << n << " is: " << find_longest_recurring_cycle(n) 
+         << endl
          << endl;
-
-    int n = 10;
-
-    for (int i = 2; i < n; ++i) {
-        double q = 1.0 / i;
-        int r = i % 1;
-        cout << "1/" << i << " = " << q << ", Remainder = " << r << endl;
-
-    }
 
     return 0;
 }
