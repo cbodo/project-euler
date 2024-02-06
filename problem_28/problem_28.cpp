82e70d3893a1af2e6ce09c50c29dada4d9abcd5f
cbodo <142287245+cbodo@users.noreply.github.com>
2024-02-06 14:13:17 -0800

    update problem_28

--- problem_28/problem_28.cpp
+++ problem_28/problem_28.cpp
@@ -21,20 +21,71 @@ Link: https://projecteuler.net/problem=28
 
 To Run:
 
-g++ problem_28.cpp -o problem_28
+g++ -std=c++11 problem_28.cpp -o problem_28
 ./problem_28
 
 */
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+void right(int row, int col) {
+	row += 0;
+	col += 1;
+}
+
+void down(int row, int col) {
+	row += 1;
+	col += 0;
+}
+
+void left(int row, int col) {
+	row += 0;
+	col -= 1;
+}
+
+void up(int row, int col) {
+	row -= 1;
+	col += 0;
+}
+
+void initialize_grid(vector<vector<int>> &grid, int side_len) {
+	for (int rows = 0; rows < side_len; ++rows) {
+    	vector<int> r;
+    	for (int cols = 0; cols < side_len; ++ cols) {
+     		r.push_back(0);
+     	}
+      	grid.push_back(r);
+    }
+}
+
+void print_grid(const vector<vector<int>> grid, int side_len) {
+	for (int r = 0; r < grid.size(); ++r) {
+    	for (int c = 0; c < grid.size(); ++ c) {
+       		cout << (grid[r][c] < 10 ? ' ' : '\0')
+         		 << grid[r][c]
+            	 << (((c + 1) % side_len) == 0 ? '\n' : ' ');
+     	}
+    }
+    cout << endl;
+}
 
 int main() {
 
     cout << endl << "Project Euler - Problem #28: Number Spiral Diagonals" << endl << endl;
 
     int sum = 0;
+    int side_len = 5;
+
+    vector<vector<int>> grid;
+    initialize_grid(grid, side_len);
+
+    int row = side_len/2;
+    int col = side_len/2;
+    grid[row][col] = 1;
+
+    print_grid(grid, side_len);
 
     cout << "The sum of diagonals in a 1001 by 1001 spiral is: " << sum << endl;
     return 0;
