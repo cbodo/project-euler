9276373a34cbbfe66ecd16b57aa5b36a23e910b3
cbodo <142287245+cbodo@users.noreply.github.com>
2024-02-07 17:11:11 -0800

    start problem_30

---
+++ problem_30/problem_30.cpp
@@ -0,0 +1,29 @@
+/*
+
+Project Euler 
+Problem #30: #30 Digit Fifth Powers - Project Euler
+Craig Bodo
+
+Description:
+Failed to fetch problem description: <urlopen error [SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: self-signed certificate in certificate chain (_ssl.c:1006)>
+
+
+Link: https://projecteuler.net/problem=30
+
+To Run:
+
+g++ problem_30.cpp -o problem_30
+./problem_30
+
+*/
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    
+    cout << endl << "Project Euler - Problem #30: #30 Digit Fifth Powers - Project Euler" << endl << endl;
+
+    return 0;
+}
