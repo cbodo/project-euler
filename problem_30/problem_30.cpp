915825842607fb93c4359a0435909d818acf0e3d
cbodo <142287245+cbodo@users.noreply.github.com>
2024-02-07 18:09:48 -0800

    start problem_30

---
+++ problem_30/problem_30.cpp
@@ -0,0 +1,37 @@
+/*
+
+Project Euler
+Problem #30: Digit Fifth Powers
+Craig Bodo
+
+Description:
+Surprisingly there are only three numbers that can be written as the sum of fourth powers of their digits:
+
+1634 = 1^4 + 6^4 + 3^4 + 4^4
+8208 = 8^4 + 2^4 + 0^4 + 8^4
+9474 = 9^4 + 4^4 + 7^4 + 4^4
+
+As 1 = 1^4 is not a sum it is not included.
+The sum of these numbers is 1634 + 8208 + 9474 = 19316.
+Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
+
+
+Link: https://projecteuler.net/problem=30
+
+To Run:
+
+g++ problem_30.cpp -o problem_30
+./problem_30
+
+*/
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+    cout << endl << "Project Euler - Problem #30: Digit Fifth Powers" << endl << endl;
+
+    return 0;
+}
