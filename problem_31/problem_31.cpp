764070bffb1cb2ca54208dfc93c01eb21cb2cdb8
cbodo <142287245+cbodo@users.noreply.github.com>
2024-02-08 12:53:14 -0800

    start problem_31

---
+++ problem_31/problem_31.cpp
@@ -0,0 +1,35 @@
+/*
+
+Project Euler
+Problem #31: Coin Sums
+Craig Bodo
+
+Description:
+In the United Kingdom the currency is made up of pound (£) and pence (p). There are eight coins in general circulation:
+1p, 2p, 5p, 10p, 20p, 50p, £1 (100p), and £2 (200p).
+
+It is possible to make £2 in the following way:
+
+    1×£1 + 1×50p + 2×20p + 1×5p + 1×2p + 3×1p
+
+How many different ways can £2 be made using any number of coins?
+
+Link: https://projecteuler.net/problem=31
+
+To Run:
+
+g++ problem_31.cpp -o problem_31
+./problem_31
+
+*/
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+    cout << endl << "Project Euler - Problem #31: Coin Sums" << endl << endl;
+
+    return 0;
+}
