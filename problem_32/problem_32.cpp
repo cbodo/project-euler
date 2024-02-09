2f8375d31ec8c01c30e4c21104f674708ad80ac7
cbodo <142287245+cbodo@users.noreply.github.com>
2024-02-09 13:08:03 -0800

    start problem_32

---
+++ problem_32/problem_32.cpp
@@ -0,0 +1,33 @@
+/*
+
+Project Euler 
+Problem #32: Pandigital Products
+Craig Bodo
+
+Description:
+We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once; for example, the 5-digit number, 15234, is 1 through 5 pandigital.
+The product 7254 is unusual, as the identity, 39 
+times 186 = 7254, containing multiplicand, multiplier, and product is 1 through 9 pandigital.
+Find the sum of all products whose multiplicand/multiplier/product identity can be written as a 1 through 9 pandigital.
+HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.
+
+
+Link: https://projecteuler.net/problem=32
+
+To Run:
+
+g++ problem_32.cpp -o problem_32
+./problem_32
+
+*/
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    
+    cout << endl << "Project Euler - Problem #32: Pandigital Products" << endl << endl;
+
+    return 0;
+}
